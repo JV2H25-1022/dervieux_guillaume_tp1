71edff93d3aaf2e80f0bdc2a5995c1a211bc3691
TheClutchPowers <dervieuxguillaume@gmail.com>
2025-02-09 16:13:13 -0500

    pleain de trucs trust

---
+++ Assets/script/rouge.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rouge : MonoBehaviour
+{
+    public GameObject objetADesactiver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        objetADesactiver.SetActive(false);
+    }
+}
