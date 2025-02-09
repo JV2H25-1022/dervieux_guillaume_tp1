1bd88d1dc7ee79bd4dacf84a030d283533862557
TheClutchPowers <dervieuxguillaume@gmail.com>
2025-02-09 12:08:22 -0500

    script changement

--- Assets/script/plateforme.cs
+++ Assets/script/plateforme.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class balle : MonoBehaviour
+public class DesactiverObjet : MonoBehaviour
 {
+    public GameObject objetADesactiver;
     // Start is called before the first frame update
-    void Start()
+    void Desactiver()
     {
-        
+        objetADesactiver.SetActive(false);
     }
 
     // Update is called once per frame
